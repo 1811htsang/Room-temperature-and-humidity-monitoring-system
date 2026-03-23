4daac850de74fa770b790383f070f6fb9c7d5fdc
QuangYS0175 <hongminhquang06@gmail.com>
2026-03-23 07:48:07 +0700

    feat(reset-driver): implement reset reason checking and software reset functionality

--- artifactspace/main/main.c
+++ artifactspace/main/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include "reset_driver.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 void app_main(void)
 {
+    printf("\n");
+    printf("====================================\n");
+    printf("ESP32 TEMPERATURE - HUMIDITY CLOCK\n");
+    printf("RESET DRIVER INITIALIZATION\n");
+    printf("====================================\n");
 
+    check_reset_reason();
+
+    vTaskDelay(10000 / portTICK_PERIOD_MS);
+
+    request_software_reset();
 }
